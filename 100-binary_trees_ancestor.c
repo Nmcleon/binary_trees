556e4fbb7ceb1db0633746c2e0b734b38da79907
Nmcleon <nmcleon10@gmail.com>
2023-11-02 17:12:51 +0200

    update

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,30 +2,25 @@
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: A pointer to the first node.
- * @second: A pointer to the second node.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: A pointer to the lowest common ancestor node, or NULL if not found.
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    if (!first || !second)
-        return (NULL);
+	binary_tree_t *mom, *pop;
 
-    /* If either of the nodes is the root, the root is the LCA. */
-    if (first == second)
-        return ((binary_tree_t *)first);
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    /* Find the lowest common ancestor in the left subtree. */
-    binary_tree_t *left_ancestor = binary_trees_ancestor(first, second->parent);
-
-    /* Find the lowest common ancestor in the right subtree. */
-    binary_tree_t *right_ancestor = binary_trees_ancestor(first->parent, second);
-
-    /* If both ancestors are not NULL, the LCA is the current node. */
-    if (left_ancestor && right_ancestor)
-        return ((binary_tree_t *)first);
-
-    /* Otherwise, return the non-NULL ancestor (if any). */
-    return (left_ancestor ? left_ancestor : right_ancestor);
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
