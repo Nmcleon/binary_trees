8961d36d4150a533edba22b9f3682e859809e19f
Nmcleon <nmcleon10@gmail.com>
2023-11-02 12:41:13 +0200

    update task 18 files

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Find uncle of a node
+ * @node: Pointer to node for which to find the uncle
+ *
+ * Return: Pointer to uncle node, else NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+        return (NULL);
+
+    if (node->parent->parent->left == node->parent)
+        return (node->parent->parent->right);
+    return (node->parent->parent->left);
+}
