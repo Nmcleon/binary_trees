7255d786e5ddf183f32f2730a307d9b0fd0c067b
Nmcleon <nmcleon10@gmail.com>
2023-11-02 05:18:32 -0700

    fix file 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    return (1 + (left_height > right_height ? left_height : right_height));
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
 
 /**
@@ -27,21 +27,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
-
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-
-    if (left_height == right_height)
-    {
-        if (tree->left == NULL && tree->right == NULL)
-            return (1);
-        if (tree->left != NULL && tree->right != NULL)
-            return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-    }
-
-    return (0);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height == right_height)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
