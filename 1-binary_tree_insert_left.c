d6175b802c89718888384d7bb181d47de5f76aba
Nmcleon <nmcleon10@gmail.com>
2023-11-02 04:55:43 -0700

    fix betty and file 5

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,29 +2,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Insert a node as the left-child of another node
- * @parent: Pointer to node to insert the left-child
- * @value: Value to store in the new node
- *
- * Return: Pointer to the created node, NULL(FAIL)/if parent == NULL
- */
+* binary_tree_insert_left - Insert a node as the left-child of another node
+* @parent: Pointer to node to insert the left-child
+* @value: Value to store in the new node
+*
+* Return: Pointer to the created node, NULL(FAIL) / if parent == NULL
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-
-    if (parent == NULL)
-        return (NULL);
-
-    new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return (NULL);
-
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
-
-    parent->left = new_node;
-    return (new_node);
+binary_tree_t *new_node;
+if (parent == NULL)
+	return (NULL);
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+	return (NULL);
+if (parent->left != NULL)
+{
+new_node->left = parent->left;
+parent->left->parent = new_node;
+}
+parent->left = new_node;
+return (new_node);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,10 +6,12 @@
  *
  * Return: 1 if node is a root, else 0
  */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
-
-    return (node->parent == NULL);
+	if (node == NULL)
+	{
+		return (0);
+	}
+	return (node->parent == NULL);
 }
